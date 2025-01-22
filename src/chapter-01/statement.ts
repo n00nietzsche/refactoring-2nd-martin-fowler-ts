76bd8780b0ede2d9978468cb767ecde1fab42a51
jakeseo <n00nietzsche@gmail.com>
2025-01-22 21:35:14 +0900

    refactor: perf -> aPerformance 루프 변수명 변경

--- src/chapter-01/statement.ts
+++ src/chapter-01/statement.ts
@@ -15,27 +15,30 @@ export function statement(
     maximumFractionDigits: 2,
   }).format;
 
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = amountFor(perf, play);
+  for (let aPerformance of invoice.performances) {
+    const play = plays[aPerformance.playID];
+    let thisAmount = amountFor(
+      aPerformance,
+      play
+    );
 
     // 포인트를 적립한다.
     volumeCredits += Math.max(
-      perf.audience - 30,
+      aPerformance.audience - 30,
       0
     );
 
     // 희극 관객 5명마다 추가 포인트를 제공한다.
     if ("comedy" === play.type) {
       volumeCredits += Math.floor(
-        perf.audience / 5
+        aPerformance.audience / 5
       );
     }
 
     // 청구 내역을 출력한다.
     result += `${play.name}: ${format(
       thisAmount / 100
-    )} ${perf.audience}석\n`;
+    )} ${aPerformance.audience}석\n`;
     totalAmount += thisAmount;
   }
 
