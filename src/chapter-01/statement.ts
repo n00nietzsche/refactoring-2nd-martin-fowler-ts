bc649b512cc040f43187f4b29e29b7fcc1006e69
Jake Seo <n00nietzsche@gmail.com>
2025-01-23 21:12:13 +0900

    refactor: 기존의 amountFor() 메서드를 클래스의 amount 로 치환

--- src/chapter-01/statement.ts
+++ src/chapter-01/statement.ts
@@ -104,7 +104,7 @@ function enrichPerformance(aPerformance: Performance) {
   return {
     ...result,
     play: playFor(result),
-    amount: amountFor(result, playFor(result)),
+    amount: calculator.amount,
     volumeCredits: volumeCreditsFor(result),
   };
 }
@@ -167,41 +167,4 @@ function volumeCreditsFor(aPerformance: Performance) {
  */
 function playFor(aPerformance: Performance) {
   return plays[aPerformance.playID as keyof Plays];
-}
-
-/**
- * 총액을 계산한다.
- * amount for -> ~에 대한 총액
- * @param aPerformance 공연 정보 (관객 수 이용)
- * @param play 장르 정보 (장르 이용)
- * @returns 총액
- */
-function amountFor(aPerformance: Performance, play: Play) {
-  let result = 0;
-
-  switch (play.type) {
-    case 'tragedy':
-      result = 40_000;
-
-      if (aPerformance.audience > 30) {
-        result += 1_000 * (aPerformance.audience - 30);
-      }
-
-      break;
-
-    case 'comedy':
-      result = 30_000;
-
-      if (aPerformance.audience > 20) {
-        result += 10_000 + 500 * (aPerformance.audience - 20);
-      }
-
-      result += 300 * aPerformance.audience;
-      break;
-
-    default:
-      throw new Error(`알 수 없는 장르: ${play.type}`);
-  }
-
-  return result;
 }
