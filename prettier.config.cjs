6d545b958b15707a8ad35cf20f8fa98fdadc0d69
Jake Seo <n00nietzsche@gmail.com>
2025-01-23 19:43:09 +0900

    chore: prettier 설정 파일 추가

---
+++ prettier.config.cjs
@@ -0,0 +1,16 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: "es5",
+    tabWidth: 2,
+    semi: true,
+    singleQuote: true,
+    printWidth: 80,
+  };
+  
+  module.exports = config;
+  
