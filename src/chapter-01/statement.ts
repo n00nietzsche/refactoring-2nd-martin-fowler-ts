ed3392b803e6eea273dca4d97df3bccd2a77755b
Jake Seo <n00nietzsche@gmail.com>
2025-01-23 19:36:46 +0900

    refactor: play-for 와 amount-for 의 함수 statement.ts 파일에 삽입

--- src/chapter-01/statement.ts
+++ src/chapter-01/statement.ts
@@ -1,6 +1,5 @@
-import { amountFor } from "./amount-for";
-import { playFor } from "./play-for";
-import type { Invoice, Play, Performance } from "./type";
+import { plays } from "./data";
+import type { Invoice, Play, Performance, Plays } from "./type";
 
 export function statement(
   invoice: Invoice,
@@ -70,3 +69,61 @@ function volumeCreditsFor(aPerformance: Performance) {
   return result;
 }
 
+/**
+ * 공연 정보를 이용해 장르 정보를 찾는다.
+ * @param aPerformance 공연 정보
+ * @param plays 장르 정보
+ * @returns 장르 정보
+ */
+function playFor(
+  aPerformance: Performance
+) {
+  return plays[
+    aPerformance.playID as keyof Plays
+  ];
+}
+
+/**
+ * 총액을 계산한다.
+ * amount for -> ~에 대한 총액
+ * @param aPerformance 공연 정보 (관객 수 이용)
+ * @param play 장르 정보 (장르 이용)
+ * @returns 총액
+ */
+function amountFor(
+  aPerformance: Performance,
+  play: Play
+) {
+  let result = 0;
+
+  switch (play.type) {
+    case "tragedy":
+      result = 40_000;
+
+      if (aPerformance.audience > 30) {
+        result +=
+          1_000 * (aPerformance.audience - 30);
+      }
+
+      break;
+
+    case "comedy":
+      result = 30_000;
+
+      if (aPerformance.audience > 20) {
+        result +=
+          10_000 +
+          500 * (aPerformance.audience - 20);
+      }
+
+      result += 300 * aPerformance.audience;
+      break;
+
+    default:
+      throw new Error(
+        `알 수 없는 장르: ${play.type}`
+      );
+  }
+
+  return result;
+}
